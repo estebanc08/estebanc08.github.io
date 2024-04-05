3c5bb7f2daf18646142320b50f69b9cdd12d942c
esteban <estebancalvo0808@gmail.com>
2024-04-05 19:17:05 -0400

    image path

--- src/static/ProjectModalText.js
+++ src/static/ProjectModalText.js
@@ -1,19 +1,19 @@
 export const SFMLProjects = {
-    imageUrl: "/images/SFMLPreview.png",
+    imageUrl: "public/images/SFMLPreview.png",
     title: "SFML Games",
     content: "Created Several Different Games and Applications using a C++ graphics library called SFML.",
     name:"SFML"
 };
 
 export const DynaCV = {
-    imageUrl: "/images/DynaCV.png",
+    imageUrl: "public/images/DynaCV.png",
     title: "Drag and Drop Resume",
     content: "Worked in a group with two other students to create a dynamic drag and drop resume builder.",
     name: "DynaCV"
 };
 
 export const UFCompass = {
-    imageUrl: "/images/Schedule.png",
+    imageUrl: "public/images/Schedule.png",
     title: "Class Scheduler",
     content: "Professional Website for fellow UF students to plan their next semester.",
     name: "UFCompass"
