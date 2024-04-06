bc9da59def0a6e856dce151d19b2449ed449c45d
esteban <estebancalvo0808@gmail.com>
2024-04-06 15:55:02 -0400

    changed page title

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,9 +2,9 @@
 import Navbar from "../components/Navbar"
 export default function Resume() {
     return(
-        <>
+        <div className="w-full">
         <Navbar />
             <iframe src="/images/resume.pdf" type="application/pdf" className="w-full h-screen" />
-        </>
+        </div>
     )
 }
