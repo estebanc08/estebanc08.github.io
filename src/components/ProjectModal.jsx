d5e4a3de215674211cf502e4b4147b8c527fa208
esteban <estebancalvo0808@gmail.com>
2024-04-06 15:39:08 -0400

    fixed styling for mobile pages

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -3,22 +3,19 @@ import { Link } from "react-router-dom"
 export default function ProjectModal({imageUrl, title, content, name}) {
     const [linkTo] = useState(`/Projects/${name}`)
     return (
-        <Link to={linkTo}>
-            <div className="h-full w-full shadow-md shadow-gray-500 rounded-xl">
-                <div className="w-full h-4/6 flex justify-center items-center">
-                    <img className="rounded-t-xl w-full h-full object-cover" src={imageUrl} />
+        <Link to={linkTo} className="h-full">
+            <div className="h-full grid grid-rows-auto-1 shadow-md shadow-gray-500 rounded-xl">
+                <div className="flex justify-center items-center">
+                    <img className="rounded-t-xl w-full h-full object-cover" src={imageUrl} alt="Image" />
                 </div>
-                <div className="h-2/6 text-black bg-white p-2 rounded-b-xl">
-                    <div className="font-bold text-3xl">
-                        {title}
-                    </div>
+                <div className="flex flex-col text-black bg-white p-2 rounded-b-xl">
+                    <div className="font-bold text-3xl">{title}</div>
                     <div className="w-full h-0.5"></div>
-                    <div>
-                        {content}
-                    </div>
+                    <div>{content}</div>
                 </div>
             </div>
         </Link>
+
     )
 
 }
