9a4c0226fb6668c93ea5e9b592a2ec8164fe59f6
esteban <estebancalvo0808@gmail.com>
2024-04-06 12:30:52 -0400

    fixed image paths for server and changed deployment branch

--- src/static/ProjectModalText.js
+++ src/static/ProjectModalText.js
@@ -1,19 +1,19 @@
 export const SFMLProjects = {
-    imageUrl: "public/images/SFMLPreview.png",
+    imageUrl: "/images/SFMLPreview.png",
     title: "SFML Games",
     content: "Created Several Different Games and Applications using a C++ graphics library called SFML.",
     name:"SFML"
 };
 
 export const DynaCV = {
-    imageUrl: "public/images/DynaCV.png",
+    imageUrl: "/images/DynaCV.png",
     title: "Drag and Drop Resume",
     content: "Worked in a group with two other students to create a dynamic drag and drop resume builder.",
     name: "DynaCV"
 };
 
 export const UFCompass = {
-    imageUrl: "public/images/Schedule.png",
+    imageUrl: "/images/Schedule.png",
     title: "Class Scheduler",
     content: "Professional Website for fellow UF students to plan their next semester.",
     name: "UFCompass"
