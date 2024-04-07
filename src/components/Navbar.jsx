c71d8b92361a523048abe41b2f88db3686c6bd58
esteban <estebancalvo0808@gmail.com>
2024-04-06 20:22:55 -0400

    adding padding to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ export default function Navbar() {
     return(
         <div className="w-full text-center py-5 flex justify-center">
             <div className="w-3/4 flex justify-center">
-                <Link className="hover:bg-gray-800 font-bold link-item w-full flex justify-center" to="/">Home</Link>
-                <Link className="hover:bg-gray-800 font-bold link-item w-full flex justify-center" to="/Projects">Projects</Link>
-                <Link className="hover:bg-gray-800 font-bold link-item w-full flex justify-center" to="/Resume">Resume</Link>
+                <Link className="p-2 sm:p-3 hover:bg-gray-800 font-bold link-item w-full flex justify-center" to="/">Home</Link>
+                <Link className="p-2 sm:p-3 hover:bg-gray-800 font-bold link-item w-full flex justify-center" to="/Projects">Projects</Link>
+                <Link className="p-2 sm:p-3 hover:bg-gray-800 font-bold link-item w-full flex justify-center" to="/Resume">Resume</Link>
             </div>
         </div>
     )
